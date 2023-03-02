3b2c3c9028ae9889b1d651c5027baaae1c2bdceb
Kunal Bhatia <113785659+KunalBhatia-Snowcraft@users.noreply.github.com>
2023-03-02 14:21:12 +0800

    Update fruitapp.py

--- fruitapp.py
+++ fruitapp.py
@@ -3,14 +3,14 @@ import streamlit as st
 def main():
     st.title("Welcome to Apple and Banana Store")
 
-    fruit = st.selectbox("Select a Fruit", ("Apples", "Bananas"))
+    fruit = st.selectbox("Select a Fruit", ("Apples ($1)", "Bananas ($0.5)"))
 
     if fruit == "Apples":
         price = 1.0
     else:
         price = 0.5
 
-    quantity = st.slider("Select Quantity", 1, 100)
+    quantity = st.slider("Select Quantity", 1, 10)
 
     total_price = price * quantity
 
