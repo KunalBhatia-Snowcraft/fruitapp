6abbcf7cff344d3098b9e0927491cc12728d3cc0
Kunal Bhatia <113785659+KunalBhatia-Snowcraft@users.noreply.github.com>
2023-03-02 14:10:40 +0800

    Create fruitapp.py

---
+++ fruitapp.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def main():
+    st.title("Welcome to Apple and Banana Store")
+
+    fruit = st.selectbox("Select a Fruit", ("Apples", "Bananas"))
+
+    if fruit == "Apples":
+        price = 1.0
+    else:
+        price = 0.5
+
+    quantity = st.slider("Select Quantity", 1, 100)
+
+    total_price = price * quantity
+
+    st.write(f"Total price for {quantity} {fruit} is ${total_price:.2f}")
+
+if __name__ == "__main__":
+    main()
