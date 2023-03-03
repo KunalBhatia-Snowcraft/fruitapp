0325687120652db622e755f8b4456fee3150deb6
Kunal Bhatia <113785659+KunalBhatia-Snowcraft@users.noreply.github.com>
2023-03-03 10:34:32 +0800

    Update fruitapp.py

--- fruitapp.py
+++ fruitapp.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(page_title='Fruit App', page_icon=':tada:', layout='wide')
+
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
+
+
 def main():
     st.title("Welcome to Apple and Banana Store")
 
